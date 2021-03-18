708b0c1a3dcbe5d36f092f073c2ae75d3f5cd4bc
yongsannnn <yong_s@live.com>
2021-03-18 06:26:41 +0000

    Added hbs

--- index.js
+++ index.js
@@ -17,6 +17,36 @@ require("handlebars-helpers")({
 
 // ROUTES BEGIN HERE
 
+async function main() {
+    // create a sql connection
+    const connection = await mysql.createConnection({
+        // host is usually the web address of the db or more commonly an ip address
+        "host": "localhost",
+        "user" : "root",
+        "database" : "sakila" 
+    });
+
+    // let query = "SELECT * FROM actor";
+    // // LINE BELOW IS ARRAY DESTRUCTURING
+    // let [actors] = await connection.execute("SELECT * FROM actor");
+    // // instead of 
+    // // let response. await connection.execute("SELECT * FROM actor")
+    // // let actors = response[0];
+    // for (let a of actors){
+    //     console.log(a)
+    // }
+    // console.log(actors.toJSON())
+
+    app.get("/actors", async (req,res)=>{
+        let [actors] = await connection.execute("SELECT * FROM actor");
+        res.render("actors",{
+            "actors": actors
+        })
+    })
+}
+
+main();
+
 app.listen(3000, () => {
   console.log("Server started");
 });
